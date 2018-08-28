555566573ed58047da548ffcdea6d3feef9ea682
Sebastian <seebzei@gmail.com>
2018-08-28 13:50:18 +0200

    Seperated Draw call for each mesh yo

--- Uke_1/main.js
+++ Uke_1/main.js
@@ -11,7 +11,6 @@ function main()
 	}
 	const shaderProgram = initShaderProgram(gl);
 	const programInfo = initProgramInfo(gl, shaderProgram);
-	const buffers = initBuffers(gl);
 
 	var then = 0;
 	function render(now) 
@@ -20,7 +19,7 @@ function main()
 		const deltaTime = now - then;
 		then = now;
 
-		drawScene(gl, programInfo, buffers, deltaTime);
+		drawScene(gl, programInfo, deltaTime);
 
 		requestAnimationFrame(render);
 	}
@@ -47,64 +46,11 @@ function initProgramInfo(gl, shaderProgram)
 }
 
 /**
- * Init buffer
- * @param {*} gl 
- */
-function initBuffers(gl) 
-{
-	// Create a buffer for the square's positions.
-
-	const positionBuffer = gl.createBuffer();
-
-	// Select the positionBuffer as the one to apply buffer
-	// operations to from here out.
-
-	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-	// Now create an array of positions for the square.
-
-	const positions =
-	[
-		//-1.0, 1.0,
-		0, 1.0,
-		//1.0, 1.0,
-		-1.0, -1.0,
-		1.0, -1.0,
-	];
-
-	// Now pass the list of positions into WebGL to build the
-	// shape. We do this by creating a Float32Array from the
-	// JavaScript array, then use it to fill the current buffer.
-
-	gl.bufferData(gl.ARRAY_BUFFER,
-		new Float32Array(positions),
-		gl.STATIC_DRAW);
-
-	const colors = 
-	[
-		1.0,  1.0,  1.0,  1.0,    // white
-		1.0,  0.0,  0.0,  1.0,    // red
-		0.0,  1.0,  0.0,  1.0,    // green
-		0.0,  0.0,  1.0,  1.0,    // blue
-	];
-	
-	const colorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-	return {
-	position: positionBuffer,
-	color: colorBuffer,
-	};
-}
-
-/**
  * Draws the scene to the canvas
  * @param {*} gl 
  * @param {*} programInfo 
- * @param {*} buffers 
  */
-function drawScene(gl, programInfo, buffers, deltatime) 
+function drawScene(gl, programInfo, deltatime) 
 {
 	gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
 	gl.clearDepth(1.0); // Clear everything
@@ -148,66 +94,7 @@ function drawScene(gl, programInfo, buffers, deltatime)
 		modelViewMatrix, // matrix to translate
 		[-0.0, 0.0, -6.0]); // amount to translate
 
-	// Tell WebGL how to pull out the positions from the position
-	// buffer into the vertexPosition attribute.
-	{
-		const numComponents = 2; // pull out 2 values per iteration
-		const type = gl.FLOAT; // the data in the buffer is 32bit floats
-		const normalize = false; // don't normalize
-		const stride = 0; // how many bytes to get from one set of values to the next
-		// 0 = use type and numComponents above
-		const offset = 0; // how many bytes inside the buffer to start from
-
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-
-		gl.vertexAttribPointer(
-			programInfo.attribLocations.vertexPosition,
-			numComponents,
-			type,
-			normalize,
-			stride,
-			offset);
-
-		gl.enableVertexAttribArray(
-			programInfo.attribLocations.vertexPosition);
-	}
-
-	// Draw color
-	{
-		const numComponents = 4;
-		const type = gl.FLOAT;
-		const normalize = false;
-		const stride = 0;
-		const offset = 0;
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-		gl.vertexAttribPointer(
-			programInfo.attribLocations.vertexColor,
-			numComponents,
-			type,
-			normalize,
-			stride,
-			offset);
-		gl.enableVertexAttribArray(
-			programInfo.attribLocations.vertexColor);
-	}
-
-	// Tell WebGL to use our program when drawing
-
-	gl.useProgram(programInfo.program);
-
-	// Set the shader uniforms
-
-	gl.uniformMatrix4fv(
-		programInfo.uniformLocations.projectionMatrix,
-		false,
-		projectionMatrix);
-
-	gl.uniformMatrix4fv(
-		programInfo.uniformLocations.modelViewMatrix,
-		false,
-		modelViewMatrix);
-
-	const offset = 0;
-	const vertexCount = 3;
-	gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+	// Draw our objects
+	let quadObject = new Quad();
+	quadObject.draw(gl, programInfo, projectionMatrix, modelViewMatrix);
 }
---
+++ Uke_1/meshes/plane.js
@@ -0,0 +1,124 @@
+
+function Quad()
+{
+    this.translation = [ 0, 0, 0 ];
+    this.rotation = [ 0, 0, 0 ];
+    this.scale = [ 1, 1, 1 ];
+
+    this.color = [ 1, 0, 0, 1];
+
+    this.vertexCount = 4;
+    this.offset = 0;
+
+    this.draw = function(gl, programInfo, projectionMatrix, modelViewMatrix)
+    {
+
+        buffers = this.initBuffer(gl);
+
+        {
+            const numComponents = 2; // pull out 2 values per iteration
+            const type = gl.FLOAT; // the data in the buffer is 32bit floats
+            const normalize = false; // don't normalize
+            const stride = 0; // how many bytes to get from one set of values to the next
+            // 0 = use type and numComponents above
+            const offset = 0; // how many bytes inside the buffer to start from
+    
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexPosition,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset);
+    
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexPosition);
+        }
+    
+        // Draw color
+        {
+            const numComponents = 4;
+            const type = gl.FLOAT;
+            const normalize = false;
+            const stride = 0;
+            const offset = 0;
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexColor,
+                numComponents,
+                type,
+                normalize,
+                stride,
+                offset);
+            gl.enableVertexAttribArray(
+                programInfo.attribLocations.vertexColor);
+        }
+    
+        // Tell WebGL to use our program when drawing
+    
+        gl.useProgram(programInfo.program);
+    
+        // Set the shader uniforms
+    
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.projectionMatrix,
+            false,
+            projectionMatrix);
+    
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.modelViewMatrix,
+            false,
+            modelViewMatrix);
+    
+        gl.drawArrays(gl.TRIANGLE_STRIP, this.offset, this.vertexCount);
+    }
+
+    this.initBuffer = function(gl)
+    {
+        // Create a buffer for the square's positions.
+
+        const positionBuffer = gl.createBuffer();
+
+        // Select the positionBuffer as the one to apply buffer
+        // operations to from here out.
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // Now create an array of positions for the square.
+
+        const positions =
+        [
+            -1.0, 1.0,
+            1.0, 1.0,
+            -1.0, -1.0,
+            1.0, -1.0,
+        ];
+
+        // Now pass the list of positions into WebGL to build the
+        // shape. We do this by creating a Float32Array from the
+        // JavaScript array, then use it to fill the current buffer.
+
+        gl.bufferData(gl.ARRAY_BUFFER,
+            new Float32Array(positions),
+            gl.STATIC_DRAW);
+
+        const colors = 
+        [
+            1.0,  1.0,  1.0,  1.0,    // white
+            1.0,  0.0,  0.0,  1.0,    // red
+            0.0,  1.0,  0.0,  1.0,    // green
+            0.0,  0.0,  1.0,  1.0,    // blue
+        ];
+        
+        const colorBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+        return {
+        position: positionBuffer,
+        color: colorBuffer,
+        };
+    }
+}
