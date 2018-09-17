baffb1eb382ff5c440bf6a4fecc962d1d8901ad9
Sebastian <seebzei@gmail.com>
2018-09-17 20:41:31 +0200

    Start of camera moving right direciton

--- Uke_2/Camera.js
+++ Uke_2/Camera.js
@@ -9,6 +9,7 @@ function Camera()
     this.zFar = 100.0;
 
     this.projectionMatrix = mat4.create();
+    this.rotationDirection = vec3.create();
 
     /**
      * Updates the projection matrix with new translation, rotation, scale values
@@ -20,13 +21,21 @@ function Camera()
 
         mat4.identity(this.projectionMatrix);
 		mat4.perspective(this.projectionMatrix, fieldOfView, aspect, this.zNear, this.zFar);
-
-  /*X*/ mat4.rotate(this.projectionMatrix, this.projectionMatrix, glMatrix.toRadian(this.rotation[0]), [1, 0, 0]);
-  /*Y*/ mat4.rotate(this.projectionMatrix, this.projectionMatrix, glMatrix.toRadian(this.rotation[1]), [0, 1, 0]);
-  /*Z*/ mat4.rotate(this.projectionMatrix, this.projectionMatrix, glMatrix.toRadian(this.rotation[2]), [0, 0, 1]);
+        
+        /*Z*/ mat4.rotate(this.projectionMatrix, this.projectionMatrix, glMatrix.toRadian(this.rotation[2]), [0, 0, 1]);
+        /*Y*/ mat4.rotate(this.projectionMatrix, this.projectionMatrix, glMatrix.toRadian(this.rotation[1]), [0, 1, 0]);
+        /*X*/ mat4.rotate(this.projectionMatrix, this.projectionMatrix, glMatrix.toRadian(this.rotation[0]), [1, 0, 0]);
 
         mat4.scale(this.projectionMatrix, this.projectionMatrix, this.scale);
-		
-        mat4.translate(this.projectionMatrix, this.projectionMatrix, this.position);
-	}
+        
+        this.rotationDirection[0] = Math.cos(glMatrix.toRadian(this.rotation[0]) * Math.cos(glMatrix.toRadian(this.rotation[1])));
+        this.rotationDirection[1] = Math.sin(glMatrix.toRadian(this.rotation[0]));
+        this.rotationDirection[2] = Math.cos(glMatrix.toRadian(this.rotation[0]) * Math.sin(glMatrix.toRadian(this.rotation[1])));
+        
+        let newPosition = [0, 0, 0];
+        let posLength = vec3.squaredLength(this.position);
+        vec3.scale(newPosition, this.rotationDirection, posLength);
+        console.log(this.rotationDirection);
+        mat4.translate(this.projectionMatrix, this.projectionMatrix, newPosition);
+    }
 }
